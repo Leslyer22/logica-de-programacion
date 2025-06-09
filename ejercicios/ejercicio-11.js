78131628372c7497559fb60c2af4e5cfc57e9fd8
Lesly <lelinha2004@gmail.com>
2025-06-09 20:40:58 -0300

    add: ejercicio 11

---
+++ ejercicios/ejercicio-11.js
@@ -0,0 +1,17 @@
+//Ejercicio 11: Contador hacia atrás
+
+/*Descripción:
+Crear un programa que muestre los números del 10 al 1 en orden descendente.
+
+Requisitos:
+
+Usar un bucle for que empiece en 10 y termine en 1.
+
+Mostrar cada número en consola o en pantalla. */
+
+
+for(let i = 10; i >= 1; i--){
+
+    console.log(i);
+
+}
